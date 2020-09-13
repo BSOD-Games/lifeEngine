80e71257e492845888c18cac892ca43ae52838d9
zombihello <zombihello@yandex.ru>
2020-09-13 22:41:45 +0300

    Возврат изменений RenderSystem.cpp в коммите 626d39c

--- Source/Runtime/Engine/Private/Rendering/RenderSystem.cpp
+++ Source/Runtime/Engine/Private/Rendering/RenderSystem.cpp
@@ -11,12 +11,6 @@
 #include "System/FileSystem.h"
 #include "OpenGL4RHI.h"
 
-le::IRHIShader*			shader;
-le::IRHIBuffer*			vertexBuffer;
-le::IRHIVertexFormat*	vertexFormat;
-le::IRHIGeometry*		geometry;
-le::IRHITexture2D*		texture2d;
-
 /**
  * Constructor
  */
@@ -65,66 +59,6 @@ bool le::RenderSystem::MakeCurrentContext( FRHIContext InRHIContext )
 	if ( rhi->MakeCurrentContext( InRHIContext ) )
 	{
 		currentContext = InRHIContext;
-		
-		shader = rhi->CreateShader();
-		vertexBuffer = rhi->CreateBuffer( BT_Vertex );
-		vertexFormat = rhi->CreateVertexFormat();
-		geometry = rhi->CreateGeometry();
-		texture2d = rhi->CreateTexture2D( IF_R_8Unorm, 1, 1 );
-
-		std::string				buffer, errorLog;
-		le::FFileHandle			file_vShader = le::FileSystem::GetInstance()->OpenFile( "Content/vertex.glsl", true );
-		if ( !file_vShader )
-		{
-			LIFEENGINE_LOG_ERROR( "Engine", "Content/vertex.glsl: not found" );
-			return false;
-		}
-
-		le::FileSystem::GetInstance()->ReadLineFromFile( file_vShader, buffer, '\0' );
-		if ( !shader->Compile( buffer, ST_Vertex, nullptr, &errorLog ) )
-		{
-			LIFEENGINE_LOG_ERROR( "Engine", "Vertex shader error: %s", errorLog.c_str() );
-			return false;
-		}
-
-		le::FileSystem::GetInstance()->CloseFile( file_vShader );
-		le::FFileHandle			file_pShader = le::FileSystem::GetInstance()->OpenFile( "Content/pixel.glsl", true );
-		if ( !file_pShader )
-		{
-			LIFEENGINE_LOG_ERROR( "Engine", "Content/pixel.glsl: not found" );
-			return false;
-		}
-
-		le::FileSystem::GetInstance()->ReadLineFromFile( file_pShader, buffer, '\0' );
-		if ( !shader->Compile( buffer, ST_Pixel, nullptr, &errorLog ) )
-		{
-			LIFEENGINE_LOG_ERROR( "Engine", "Pixel shader error: %s", errorLog.c_str() );
-			return false;
-		}
-
-		le::FileSystem::GetInstance()->CloseFile( file_pShader );
-		if ( !shader->Link( &errorLog ) )
-		{
-			LIFEENGINE_LOG_ERROR( "Engine", "Link shader error: %s", errorLog.c_str() );
-			return false;
-		}
-
-		uint8 pixel = 255;
-		texture2d->Allocate( &pixel );
-
-		float		verteces[] =
-		{
-			-0.5f, -0.5f, 0.0f, 1.0f, 1.0f,
-			0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-			0.0f,  0.5f, 0.0f, 0.0f, 0.0f
-		};
-
-		vertexBuffer->Allocate( verteces, sizeof( verteces ) );
-		vertexFormat->PushFloat( 3 );
-		vertexFormat->PushFloat( 2 );
-		geometry->SetVertexBuffer( vertexBuffer );
-		geometry->SetVertexFormat( vertexFormat );
-
 		return true;
 	}
 
@@ -159,13 +93,6 @@ void le::RenderSystem::End()
 void le::RenderSystem::Present()
 {
 	if ( !currentContext ) return;
-	
-	rhi->SetShader( shader );
-	rhi->SetGeometry( geometry );
-	rhi->SetTexture2D( texture2d );
-	rhi->Draw( DO_TriangleList, 0, 3 );
-	rhi->SetGeometry( nullptr );
-
 	rhi->SwapBuffers( currentContext );
 }
 
